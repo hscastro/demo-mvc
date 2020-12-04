be9875bc4d32e413aeafc62c4d081e633c99f977
hscastro <halisonsc5@gmail.com>
2020-12-04 16:24:34 -0300

    codificação do listar departamentos

--- src/main/java/br/ce/hscastro/controllers/DepartamentoController.java
+++ src/main/java/br/ce/hscastro/controllers/DepartamentoController.java
@@ -2,7 +2,9 @@ package br.ce.hscastro.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.ce.hscastro.domain.Departamento;
@@ -16,16 +18,17 @@ public class DepartamentoController {
 	private DepartamentoService service;
 	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
-		return "/departamentos/cadastrar";
+	public String cadastrar(Departamento departamento) {
+		return "/departamento/cadastro";
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
-		return "/departamentos/lista";
+	public String listar(ModelMap model) {
+		model.addAttribute("departamentos", service.buscarTodos());
+		return "/departamento/lista";
 	}
 	
-	@GetMapping("/salvar")
+	@PostMapping("/salvar")
 	public String salvar(Departamento departamento) {
 		service.salvar(departamento);
 		return "redirect:/departamentos/cadastrar";
